44ac489057de42a1f1b869f771befa63588e96d3
CatsJuice <wp1298554944@hotmail.com>
2023-07-13 09:20:25 +0800

    init project structure

---
+++ packages/playground/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: 'Dockbar - Playground',
+  meta: [
+    { name: 'description', content: 'macOS-like dock component made by web-components' },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#F77D5C' : '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => preferredDark.value ? '/dockbar.svg' : '/dockbar.svg',
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ packages/playground/src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div bg-red text-white>
+    <h1>{{ t('hello') }}</h1>
+  </div>
+</template>
